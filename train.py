9e526ba9802d14b71dc9d4b48507718dc06dc8e3
SampannaKahu <sampyash@gmail.com>
2019-11-18 00:22:40 -0500

    Working model.

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ import pandas as pd
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torchviz import make_dot, make_dot_from_trace
 
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -28,6 +29,8 @@ print("torch==%s" % torch.version.git_version)
 print("matplotlib==%s" % json.loads(matplotlib._version.version_json)['full-revisionid'])
 
 seed = 42
+device = utils.DEVICE
+torch.cuda.empty_cache()
 
 if __name__ == '__main__':
     # set random seed to 0
@@ -40,19 +43,27 @@ if __name__ == '__main__':
     # Load and preprocess data.
     df = pd.read_csv("data/scheduling_data_out_ab_nginx.csv")
     df = utils.preprocess_data(df=df)
+    dataa = df.to_numpy()
+    # dataa = dataa.reshape(197, 28, -1)
+    dataa = dataa.reshape(985, 28, -1)
+    dataa = np.transpose(dataa, axes=(0, 2, 1))
 
     assert df.isnull().values.any() == False, "Dataset contains a NaN value. Aborting."
 
     assert df.apply(lambda s: pd.to_numeric(s, errors='coerce').notnull().all()).all(), \
         "At least 1 value in the dataframe is non-numeric."
 
-    train_x, train_y, test_x, test_y = utils.make_training_and_testing_set(data, percent_train=97.0)
+    # train_x, train_y, test_x, test_y = utils.make_training_and_testing_set(data, percent_train=97.0)
+    train_x, train_y, test_x, test_y = utils.make_training_and_testing_set(dataa, percent_train=97.0)
+    train_x, train_y, test_x, test_y = train_x.to(device), train_y.to(device), test_x.to(device), test_y.to(device)
 
     # build the model
-    seq = model.Sequence()
+    # seq = model.Sequence()
+    seq = model.Sequence2(in_dim=28, out_dim=28, hidden_dim=51).to(device)
     seq.double()
 
-    criterion = nn.MSELoss()
+
+    criterion = nn.MSELoss().to(device)
     # use LBFGS as optimizer since we can load the whole data to train
     optimizer = optim.LBFGS(seq.parameters(), lr=0.8)
 
@@ -63,8 +74,8 @@ if __name__ == '__main__':
 
         def closure():
             optimizer.zero_grad()
-            out = seq(train_x)
-            loss = criterion(out, train_y)
+            out = seq(train_x).to(device)
+            loss = criterion(out, train_y).to(device)
             print('loss:', loss.item())
             loss.backward()
             return loss
@@ -77,7 +88,7 @@ if __name__ == '__main__':
             pred = seq(test_x, future=future)
             loss = criterion(pred[:, :-future], test_y)
             print('test loss:', loss.item())
-            y = pred.detach().numpy()
+            y = pred.cpu().numpy()
         # draw the result
         plt.figure(figsize=(30, 10))
         plt.title('Predict future values for time sequences\n(Dashlines are predicted values)', fontsize=30)
