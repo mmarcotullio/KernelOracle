9cb9135104e14ae2ccd6f4dd49c93662b7b8adbf
Madhav Rajesh <madmusic2020@gmail.com>
2026-02-17 01:32:55 -0800

    added modifications to data_preprocessing and train and utils

--- baseline/data_preprocessing.py
+++ baseline/data_preprocessing.py
@@ -6,16 +6,18 @@ with open("./data/" + input_file_path_name + ".txt") as data_file:
 lookup_map = {}
 
 with open("./data/scheduling_data_" + input_file_path_name + ".csv", mode="w") as schedulind_Data:
-    schedulind_Data.write("task_code,time,name,pid" + "\n")
+    schedulind_Data.write("task_code,time,name,pid\n")
+
     for line in lines:
         tokens = [token for token in line.split(" ") if token]
 
-        if len(tokens) is not 4 and len(tokens) is not 6:
+        if len(tokens) != 4 and len(tokens) != 6:
             print(tokens)
             raise ValueError("Length of row does not seem right! Stopping.")
 
-        code = tokens[0].strip("*").strip(" ").strip("*")
-        if len(tokens) is 6:
+        code = tokens[0].strip("*").strip(" ")
+
+        if len(tokens) == 6:
             name = tokens[5].strip("\n").split(":")[0]
             pid = tokens[5].strip("\n").split(":")[1]
             lookup_map[code] = (name, pid)
